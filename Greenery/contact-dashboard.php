4a0cb6eccab22e1797d078dc11676f444fa78f0d
Obiasfate <79243642+Obiasfate@users.noreply.github.com>
2022-07-21 20:09:21 +0800

    Update contact-dashboard.php

--- Greenery/contact-dashboard.php
+++ Greenery/contact-dashboard.php
@@ -1,35 +1,31 @@
 <?php
 
-@include 'dbh.inc.php';
+    include 'includes/dbh.inc.php';
 
-if(isset($_POST['add_product'])) {
+    if (isset($_POST['add_product'])) {
 
-  $product_name = $_POST['product_name'];
-  $product_price = $_POST['product_price'];
-  $product_image = $_FILES['product_image']['name'];
-  $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
-  $product_image_folder = 'uploaded_img/' .$product_image;
+        $product_name = $_POST['product_name'];
+        $product_price = $_POST['product_price'];
+        $product_image = $_FILES['product_image']['name'];
+        $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
+        $product_image_folder = 'uploaded_img/' . $product_image;
 
-  if(empty($product_name) || empty($product_price) || empty($product_image)) {
-    $message[] = 'Please Fill Out All Field';
-  }
-  else {
-    $insert = "INSERT INTO product_info(product_name, product_price, product_image) VALUES('$product_name', '$product_price', '$product_image')";
-    $upload = mysqli_query($conn, $insert);
-    if($upload) {
-      move_uploaded_file($product_image_tmp_name, $product_image_folder);
-      $message[] = 'New Product Addedd Successfully!';
+        if (empty($product_name) || empty($product_price) || empty($product_image)) {
+            $message[] = 'Please Fill Out All Field';
+        } else {
+            $insert = "INSERT INTO `product_info`(`product_name`, `product price`, `product_image`) VALUES('$product_name', '$product_price', '$product_image')";
+            $upload = mysqli_query($conn, $insert);
+            if ($upload) {
+                move_uploaded_file($product_image_tmp_name, $product_image_folder);
+                $message[] = 'New Product Added Successfully!';
+            } 
+            else {
+                $message[] = 'Could not Add the Product :(';
+            }
+        }
     }
-    else {
-      $message[] = 'Could not Add the Product :(';
-    }
-  }
-
-}
 
 ?>
-
-
 
 
 <!DOCTYPE html>
@@ -46,9 +42,7 @@ if(isset($_POST['add_product'])) {
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
@@ -69,10 +63,8 @@ if(isset($_POST['add_product'])) {
     <header class="d-flex flex-wrap justify-content-center py-4 mb-4 border-bottom">
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="admin-dashboard.php"><img src="assets/img/greenery_logo.png" alt=""
-                        width="30" height="24" class="d-inline-block align-text-top">Greenery</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="admin-dashboard.php"><img src="assets/img/greenery_logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">Greenery</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
@@ -98,32 +90,31 @@ if(isset($_POST['add_product'])) {
             </ol>
             <h1 class="mt-4 py-3">Product Dashboard</h1>
 
-<!-- PRODUCT-CRUD -->
+            <!-- PRODUCT-CRUD -->
 
-<?php
+            <?php
 
-if(isset($message)){
-  foreach($message as $message){
-    echo '<span class="message">'.$message.'</span>';
-  }
-}
+            if (isset($message)) {
+                foreach ($message as $message) {
+                    echo '<span class="message">' . $message . '</span>';
+                }
+            }
 
-?>
+            ?>
 
             <div class="container">
 
-              <div class="product-form-container">
+                <div class="product-form-container">
 
-                  <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
-                      <h3>ADD NEW PRODUCT</h3>
-                      <input type="text" placeholder="Enter product name..." name="product_name" class="box">
-                      <input type="number" placeholder="Enter product price..." name="product_price" class="box">
-                      <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image"
-                          class="box">
-                      <input type="submit" class="btn" name="add_product" value="Add a Product">
-                  </form>
+                    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+                        <h3>ADD NEW PRODUCT</h3>
+                        <input type="text" placeholder="Enter product name..." name="product_name" class="box">
+                        <input type="number" placeholder="Enter product price..." name="product_price" class="box">
+                        <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image" class="box">
+                        <input type="submit" class="btn" name="add_product" value="Add a Product">
+                    </form>
 
-              </div>
+                </div>
 
 
 
@@ -137,9 +128,9 @@ if(isset($message)){
 
                 <div class="card-body">
                     <?php
-$sql = "SELECT * FROM `main`";
-$result = $conn->query($sql);
-?>
+                    $sql = "SELECT * FROM `main`";
+                    $result = $conn->query($sql);
+                    ?>
                     <table id="datatablesSimple">
                         <thead>
                             <tr>
@@ -152,18 +143,18 @@ $result = $conn->query($sql);
                         </thead>
                         <tbody>
                             <?php
-while ($row = $result->fetch_assoc()) {
-?>
-                            <tr>
-                                <td><?php echo $row['name']; ?></td>
-                                <td><?php echo $row['email']; ?></td>
-                                <td><?php echo $row['contactnumber']; ?></td>
-                                <td><?php echo $row['schoolname']; ?></td>
-                                <td><?php echo $row['message']; ?></td>
-                            </tr>
+                            while ($row = $result->fetch_assoc()) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $row['name']; ?></td>
+                                    <td><?php echo $row['email']; ?></td>
+                                    <td><?php echo $row['contactnumber']; ?></td>
+                                    <td><?php echo $row['schoolname']; ?></td>
+                                    <td><?php echo $row['message']; ?></td>
+                                </tr>
                             <?php
-}
-?>
+                            }
+                            ?>
                         </tbody>
                     </table>
                 </div>
